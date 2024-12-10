af809e7005f37e45e06407cd25c989f69c9c0af1
Hugo PETIT <hugo.petitleneuder@gmail.com>
2024-12-10 16:21:33 +0100

    cdn fullcalendar

--- frontend/src/views/PlanningView.vue
+++ frontend/src/views/PlanningView.vue
@@ -1,4 +1,16 @@
+<script setup>
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth'
+  });
+  calendar.render();
+});
+
+</script>
+
 <template>
     <h1>Planning</h1>
+    <div id='calendar'></div>
   </template>
-  
